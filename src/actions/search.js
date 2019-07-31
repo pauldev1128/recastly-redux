17d29c21289c56893410875214348a72b5284b89
pauldev1128 <pauldev1128@gmail.com>
2019-07-31 12:40:14 -0700

    created changeVideos and VideoList actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,12 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  return (
+    type: "HANDLE_VIDEO_SEARCH",
+    q: videos
+  )
 };
 
 export default handleVideoSearch;
